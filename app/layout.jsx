815cdc54e6e1ff2dda4fc0b17882ab46972ba429
shiroee <hobiesantos@licaventures.com>
2025-01-14 19:03:06 +0800

    feat: added ico

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import './globals.css';
 import localFont from 'next/font/local'
 
@@ -6,15 +7,38 @@ const weddingFont = localFont({
   display: 'swap',
   variable: '--font-wedding-font',
 })
+export const metadata = {
+  title: 'Paul & Rica',
+  description: 'Paul and Rica are getting married!',
+  icons: {
+    icon: [
+        { url: '/ico32x32.ico', sizes: '32x32', type: 'image/png'},
+        { url: '/ico192x192.ico', sizes: '192x192', type: 'image/png'},
+    ],
+    shortcut: '/ico32x32.ico',
+    apple: [
+        { url: '/ico32x32.ico', sizes: '32x32', type: 'image/png'},
+        { url: '/ico192x192.ico', sizes: '192x192', type: 'image/png'},
+    ],
+    other: {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/ico32x32.ico',
+    },
+},
+};
 
 export default function RootLayout({ children}) {
   return (
     <html lang="en" className={weddingFont.variable}>
-      <video autoPlay loop muted playsInline className="fixed inset-0 w-full h-full object-cover z-[-1]">
-        <source src={'/bg/vbg.mp4'} type="video/mp4"/>
-          Your browser does not support the video tag.
-      </video>
-      <body>{children}</body>
+      <body>
+        <Suspense>
+          <video autoPlay loop muted playsInline className="fixed inset-0 w-full h-full object-cover z-[-1]">
+            <source src={'/bg/vbg.mp4'} type="video/mp4"/>
+              Your browser does not support the video tag.
+          </video>
+        </Suspense>
+        {children}
+      </body>
     </html>
   )
 }
